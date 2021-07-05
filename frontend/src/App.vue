23d8472b0b3b3f5a821ff3b6478dec0ae5f2c733
OIer0528 <814603030@qq.com>
2021-07-05 23:45:23 +0800

    Import Element-Plus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-container>
+      <el-header>
+        <h1>第一个 Three.js 示例项目</h1>
+      </el-header>
+      <el-main> </el-main>
+      <el-footer> <Footer /> </el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+#app .el-header {
+  background: rgb(83, 168, 255);
+  color: #ffffff;
+}
+
+#app .el-footer {
+  background: rgb(179, 216, 255);
+  position: fixed;
+  width: 100%;
+  bottom: 0;
 }
 </style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <p>
+      项目地址：
+      <el-link href="https://github.com/OIer0528/FirstThree">
+        <img alt="GitHub Mark" src="../assets/GitHub-Mark-32px.png" />
+        OIer0528/FirstThree
+      </el-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+}
+</script>
+
+<style scoped>
+.el-link {
+  font-size: 1rem;
+}
+
+.el-link img {
+  height: 1rem;
+}
+</style>
